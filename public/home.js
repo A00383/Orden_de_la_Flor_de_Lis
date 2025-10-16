0b22ef3c522807741c9b20e8acf6131599f7ad87
A00383 <a00383@mariareinamarianistas.net>
2025-10-16 00:50:57 -0500

    .

--- public/home.js
+++ public/home.js
@@ -1,9 +1,40 @@
 const homeswitch = document.getElementById("home");
-
+const historyswitch = document.getElementById("history");
+const socialworkswitch = document.getElementById("socialwork");
+const membersswitch = document.getElementById("members");
+const findusswitch = document.getElementById("findus");
 
 if (homeswitch) {
     homeswitch.addEventListener("click", () => {
         const homeurl = `https://ordendelaflordelis.vercel.app/index.html`;
         window.open(homeurl, "_blank");
+    });
+}
+
+if (historyswitch) {
+    historyswitch.addEventListener("click", () => {
+        const historyurl = `https://ordendelaflordelis.vercel.app/history.html`;
+        window.open(historyurl, "_blank");
+    });
+}
+
+if (socialworkswitch) {
+    socialworkswitch.addEventListener("click", () => {
+        const socialworkurl = `https://ordendelaflordelis.vercel.app/socialwork.html`;
+        window.open(socialworkurl, "_blank");
+    });
+}
+
+if (membersswitch) {
+    membersswitch.addEventListener("click", () => {
+        const membersurl = `https://ordendelaflordelis.vercel.app/members.html`;
+        window.open(membersurl, "_blank");
+    });
+}
+
+if (findusswitch) {
+    findusswitch.addEventListener("click", () => {
+        const findusurl = `https://ordendelaflordelis.vercel.app/findus.html`;
+        window.open(findusurl, "_blank");
     });
 }
